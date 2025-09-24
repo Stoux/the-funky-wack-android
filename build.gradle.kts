87d077e8a86536554c56165e13b5862a52b9345d
Leon Stam <leon@melonats.dev>
2025-09-24 13:23:38 +0200

    Centralize JVM / Kotlin version(s) via Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,17 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+// Configure all subprojects that use the Kotlin Android plugin
+subprojects {
+    afterEvaluate {
+        plugins.withId("org.jetbrains.kotlin.android") {
+            the<org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension>().jvmToolchain(17)
+        }
+
+        // Also configure it for pure JVM libraries like :core:common might be
+        plugins.withId("org.jetbrains.kotlin.jvm") {
+            the<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension>().jvmToolchain(17)
+        }
+    }
+}
