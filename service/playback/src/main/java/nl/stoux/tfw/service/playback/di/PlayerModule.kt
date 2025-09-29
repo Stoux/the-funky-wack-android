734787a1a6fdea789926f988bc63059e304e644b
Leon Stam <leon@melonats.dev>
2025-09-29 16:52:45 +0200

    Improve default ExoPlayer configuration

--- service/playback/src/main/java/nl/stoux/tfw/service/playback/di/PlayerModule.kt
+++ service/playback/src/main/java/nl/stoux/tfw/service/playback/di/PlayerModule.kt
@@ -1,8 +1,12 @@
 package nl.stoux.tfw.service.playback.di
 
 import android.content.Context
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C.WAKE_MODE_LOCAL
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.SeekParameters
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,6 +22,16 @@ object PlayerModule {
     @Singleton
     @UnstableApi
     fun provideExoPlayer(@ApplicationContext context: Context): ExoPlayer {
-        return ExoPlayer.Builder(context).build()
+        val player = ExoPlayer.Builder(context)
+            .setSeekParameters(SeekParameters.CLOSEST_SYNC)
+            .setWakeMode(WAKE_MODE_LOCAL)  /* Prevent the service from being killed during playback */
+            .setHandleAudioBecomingNoisy(true)
+            .setAudioAttributes(AudioAttributes.DEFAULT, true)
+            .build()
+
+        player.repeatMode = Player.REPEAT_MODE_ALL;
+
+        return player;
     }
+
 }
