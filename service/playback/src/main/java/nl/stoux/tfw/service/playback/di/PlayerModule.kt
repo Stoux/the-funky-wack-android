ee56d554be5b1fb405d5cde9cdaac25d54d446f6
Leon Stam <leon@melonats.dev>
2025-09-29 17:21:25 +0200

    Fix other audio apps not stopping when starting playback

--- service/playback/src/main/java/nl/stoux/tfw/service/playback/di/PlayerModule.kt
+++ service/playback/src/main/java/nl/stoux/tfw/service/playback/di/PlayerModule.kt
@@ -2,6 +2,7 @@ package nl.stoux.tfw.service.playback.di
 
 import android.content.Context
 import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
 import androidx.media3.common.C.WAKE_MODE_LOCAL
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
@@ -22,11 +23,17 @@ object PlayerModule {
     @Singleton
     @UnstableApi
     fun provideExoPlayer(@ApplicationContext context: Context): ExoPlayer {
+        // Correctly take audio priority over other apps when playing starts
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .setUsage(C.USAGE_MEDIA)
+            .build()
+
         val player = ExoPlayer.Builder(context)
             .setSeekParameters(SeekParameters.CLOSEST_SYNC)
             .setWakeMode(WAKE_MODE_LOCAL)  /* Prevent the service from being killed during playback */
             .setHandleAudioBecomingNoisy(true)
-            .setAudioAttributes(AudioAttributes.DEFAULT, true)
+            .setAudioAttributes(audioAttributes, true)
             .build()
 
         player.repeatMode = Player.REPEAT_MODE_ALL;
