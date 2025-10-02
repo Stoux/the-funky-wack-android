d34c3803f89021076c5244d186e92c761f88b278
Leon Stam <leon@melonats.dev>
2025-10-02 16:53:26 +0200

    Nuke CarAppService logic: We're going Audio service only.

--- automotive/src/main/java/nl/stoux/tfw/automotive/di/ModelFactories.kt
+++
@@ -1,9 +0,0 @@
-package nl.stoux.tfw.automotive.di
-
-import dagger.assisted.AssistedFactory
-import nl.stoux.tfw.automotive.car.TfwCarSession
-
-@AssistedFactory
-interface TfwCarSessionFactory {
-    fun create(): TfwCarSession
-}
